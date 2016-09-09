e66aec9f5b8db518b59f456d7b3d4b6827974b5b
enricoCortes <enricocortes93@gmail.com>
2016-09-09 16:32:49 +0200

    Create ammHomeView.php

---
+++ view/amm/ammHomeView.php
@@ -0,0 +1,10 @@
+
+<?php   
+		include('inc/header-top-sidebar-content.php'); 
+
+		include('inc/amm/ammTopBar.php'); 
+
+		include('inc/amm/listaOperazioni.php'); 
+	
+		include('inc/footer.php'); 
+?> 
