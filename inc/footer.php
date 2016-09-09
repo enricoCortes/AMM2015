4b786d6ecb407dea859440f887dd4aa6b212f464
enricoCortes <enricocortes93@gmail.com>
2016-09-09 16:28:37 +0200

    Create footer.php

---
+++ inc/footer.php
@@ -0,0 +1,19 @@
+
+	</div> <!-- cjiusura div content -->
+
+	
+	<div id = "footer">
+		<hr/>
+		<p> Sviluppato da <b> Enrico Cortes </b> <br/>
+			<b>Matricola</b>: 48942 <br/>
+			<b>email</b>:  EnricoSabbath@yahoo.com <br/>
+			<b>email2</b>:  EnricoCortes93@gmail.com
+		</p>
+		<hr/>
+	</div>
+	
+	
+
+	</body>
+
+</HTML>
