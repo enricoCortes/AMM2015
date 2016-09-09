3a9eabf5b3a08527db852bea1ae28c25785c33c1
enricoCortes <enricocortes93@gmail.com>
2016-09-09 16:33:00 +0200

    Add files via upload

---
+++ view/amm/listaInserzioniView.php
@@ -0,0 +1,11 @@
+
+
+<?php   
+		include('inc/header-top-sidebar-content.php'); 
+
+		include('inc/amm/ammTopBar.php'); 
+
+		include('inc/inserzioni.php'); 
+	
+		include('inc/footer.php'); 
+?> 
---
+++ view/amm/manageUsersView.php
@@ -0,0 +1,11 @@
+
+
+<?php   
+		include('inc/header-top-sidebar-content.php'); 
+
+		include('inc/amm/ammTopBar.php'); 
+
+		include('inc/amm/userList.php'); 
+	
+		include('inc/footer.php'); 
+?> 
