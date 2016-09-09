2f57dfb20585a6b759c972774123f8beb15d3120
enricoCortes <enricocortes93@gmail.com>
2016-09-09 16:35:42 +0200

    Create accountManagerView.php

---
+++ view/user/accountManagerView.php
@@ -0,0 +1,9 @@
+
+
+<?php include('inc/header-top-sidebar-content.php'); ?>
+
+<?php include('inc/user/userTopBar.php'); ?> 
+
+<?php include('inc/user/accountManager.php'); ?> 
+	
+<?php include('inc/footer.php'); ?> 
